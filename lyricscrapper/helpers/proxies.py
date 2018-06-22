485f962e7255086618ba7ec7f9bb139ede85bab1
Marcelo Carrillo <marcelocarrilloestevez@gmail.com>
2018-06-21 22:56:51 -0400

    Proxies modified

--- lyricscrapper/helpers/proxies.py
+++ lyricscrapper/helpers/proxies.py
@@ -4,11 +4,10 @@ from bs4 import BeautifulSoup
 
 def main():
     candidate_proxies = ['localhost:8118']
-    for proxy in candidate_proxies:
-
-        print ("Trying HTTP proxy %s" % proxy)
-        try:
-            while True:
+    while True:
+        for proxy in candidate_proxies:
+            print ("Trying HTTP proxy %s" % proxy)
+            try:
                 #create the object, assign it to a variable
                 proxy_req = urllib.request.ProxyHandler({'http': proxy})
                 # construct a new opener using your proxy settings
@@ -21,9 +20,9 @@ def main():
                 print("External IP:  " + str(soup))
                 #break
                 time.sleep(60)
-        except Exception as e:
-            print ("Trying next proxy in 5 seconds: " + str(e))
-            time.sleep(5)
+            except Exception as e:
+                print ("Trying next proxy in 5 seconds: " + str(e))
+                time.sleep(5)
 
 if __name__ == '__main__':
     main()
