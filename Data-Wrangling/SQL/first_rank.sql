7b2fae883ec574dd2ea2c45b914a2878644ce0f8
Kate Sebby <kate.sebby@gmail.com>
2018-08-14 17:15:20 -0400

    "repo organization"

--- Data-Wrangling/SQL/first_rank.sql
+++
@@ -1,3 +0,0 @@
-select artist, song, ranking, min(convert(semana, date)) as min_date
-    from musicmood.billboard_ranking
-    group by artist, song
--- Data-Wrangling/SQL/unique_1_by_artist.sql
+++
@@ -1,9 +0,0 @@
-select artist, 
-		  #count(artist) as 'weekly_appearances',
-          sum(case when ranking=1 then 1 else 0 end) as 'weeks_at_number_1',
-          count(distinct(song)) as 'number_1s'
-from musicmood.billboard_ranking
-where ranking='1'
-
-group by artist
-order by 3 desc
